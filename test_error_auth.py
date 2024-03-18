b56361c1c38abe3bd32677598a8b00d26adf2f64
rommeisner <rommeisner@yandex.ru>
2024-03-19 01:26:22 +0300

    my not first autotest

--- test_error_auth.py
+++ test_error_auth.py
@@ -7,9 +7,11 @@ driver = webdriver.Chrome()
 LOGIN_BUTTON = (By.XPATH, '//*[@aria-label="Войти"]')
 EMAIL_FIELD = (By.XPATH, '//*[@type="email"]')
 NEXT_BUTTON = (By.XPATH, '//*[@id="identifierNext"]')
-# ERROR_TEXT = (By.XPATH, '//*[text()="Не удалось найти аккаунт Google."]')
 ERROR_TEXT = (By.XPATH, '//*[text()="Введите адрес электронной почты или номер телефона."]')
-
+MAIL_BUTTON = (By.XPATH, '//*[@aria-label="Почта (откроется новая вкладка)"]')
+SIGH_IN = (By.XPATH, '//*[@data-action="sign in"]')
+PICTURE_BUTTON = (By.XPATH, '//*[@aria-label="Поиск картинок (откроется новая вкладка)"]')
+GOOGLE_PICTURE = (By.XPATH,'//*[@class="T8VaVe"]')
 
 def find_element(driver, locator, time=5):
     return WebDriverWait(driver, time).until(expected_conditions.presence_of_element_located(locator))
@@ -20,6 +22,22 @@ def test_error_auth():
     # find_element(driver, EMAIL_FIELD).send_keys("mail")
     find_element(driver,NEXT_BUTTON).click()
     find_element(driver, ERROR_TEXT)
+
+def test_error_email():
+    driver.get("https://google.com")
+    find_element(driver, MAIL_BUTTON).click()
+    find_element(driver, SIGH_IN).click()
+    find_element(driver, NEXT_BUTTON).click()
+    find_element(driver,ERROR_TEXT)
+
+def test_text_picture():
+    driver.get("https://google.com")
+    find_element(driver, PICTURE_BUTTON).click()
+    find_element(driver,GOOGLE_PICTURE)
+
+
+
+
 
 
 
